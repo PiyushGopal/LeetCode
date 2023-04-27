dfc0b781d0962358859f10ea6766588ff81306b8
Piyush Gopal <99497706+PiyushGopal@users.noreply.github.com>
2023-04-27 20:47:59 +0530

    Create 13. Roman to Integer.py

---
+++ 13. Roman to Integer.py
@@ -0,0 +1,48 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        d={
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000
+        }
+        sum=0
+        flag=0
+        for i in range(len(s)):
+            if(flag==1):
+                flag=0
+                continue
+            if i!=len(s)-1:
+                if(s[i]=='I' and s[i+1]=='V'):
+                    sum+=4
+                    break
+                elif(s[i]=='I' and s[i+1]=='X'):
+                    sum+=9
+                    break
+                elif(s[i]=='X' and s[i+1]=='L'):
+                    sum+=40
+                    flag=1
+                elif(s[i]=='X' and s[i+1]=='C'):
+                    sum+=90
+                    flag=1
+                elif(s[i]=='C' and s[i+1]=='D'):
+                    sum+=400
+                    flag=1
+                elif(s[i]=='C' and s[i+1]=='M'):
+                    sum+=900
+                    flag=1
+                else:
+                    sum+=d[s[i]]
+            else:
+                sum += d[s[i]]
+
+
+        return sum
+            
