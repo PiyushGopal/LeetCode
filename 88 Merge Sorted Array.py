83c0ed30ee04c914db7ec620e5e511be9955c827
Piyush Gopal <99497706+PiyushGopal@users.noreply.github.com>
2023-05-20 14:59:41 +0530

    Create 88 Merge Sorted Array.py

---
+++ 88 Merge Sorted Array.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        temp=head
+        if not head:
+            return
+        while temp.next: 
+            if temp.val == temp.next.val:
+                temp.next=temp.next.next
+            else:
+                temp=temp.next
+            if temp == None:
+                break
+        return head
