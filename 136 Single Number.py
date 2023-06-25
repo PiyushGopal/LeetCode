76b8ce103ea586860dba6ae8e1c27061fb35eba3
Piyush Gopal <99497706+PiyushGopal@users.noreply.github.com>
2023-06-25 17:14:55 +0530

    Create 136 Single Number.py

---
+++ 136 Single Number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        """ #sol1
+        for i in nums:
+            if nums.count(i)==1:
+                return i
+                """
+        
+        res=0
+        for i in  nums:
+            res = i ^ res
+        return res
